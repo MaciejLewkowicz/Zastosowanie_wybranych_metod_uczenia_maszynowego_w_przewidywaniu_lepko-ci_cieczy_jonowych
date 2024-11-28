6fb07e1b1c42f86f3bbacc3b5b930fb8eba81781
MaciejLewkowicz <maciej.lewkowicz.stud@pw.edu.pl>
2024-11-29 00:16:11 +0100

    feature selection

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import r_regression, SequentialFeatureSelector
 from sklearn import svm
 import math
@@ -9,47 +9,46 @@ import math
 TRAIN_DIR = "./train"
 os.path.exists(TRAIN_DIR) or os.makedirs(TRAIN_DIR)
 
-t_data = pd.read_csv("training_data.csv")
+if not os.path.exists(f"{TRAIN_DIR}/data_after_correaltion.csv"):
+    t_data = pd.read_csv("training_data.csv")
+    print(t_data.shape)
+    correlation = t_data.loc[:, t_data.columns[t_data.columns != "Viscosity"]].corr() > 0.5
+
+    print("correlation:", correlation.shape)
+    features_to_drop = []
+
+    for i, j in [(i, j) for i in range(0, correlation.shape[0]) for j in range(0, i)]:
+            if correlation.iloc[i, j]:
+                rsqa, rsqb = r_regression(t_data.iloc[:, [i, j]], t_data["Viscosity"])
+                if abs(rsqa) > abs(rsqb): features_to_drop.append(i)
+                else: features_to_drop.append(j)
+
+    t_data = t_data.loc[:, [not i in features_to_drop or t_data.columns[i] == "Viscosity" for i in range(0, t_data.shape[1])]]
+    t_data.to_csv(f"{TRAIN_DIR}/data_after_correaltion.csv")
+else:
+    t_data = pd.read_csv(f"{TRAIN_DIR}/data_after_correaltion.csv")
+
 print(t_data.shape)
-ranking = t_data.loc[:, t_data.columns[t_data.columns != "Viscosity"]].describe().transpose().sort_values("std").index.to_numpy()[:500]
-correlation = t_data.loc[:, t_data.columns[t_data.columns != "Viscosity"]].corr() > 0.5
 
-print("correlation:", correlation.shape)
-features_to_drop = []
+rsqs = list()
 
-for i, j in [(i, j) for i in range(0, correlation.shape[0]) for j in range(0, i)]:
-        if correlation.iloc[i, j]:
-            rsqa, rsqb = r_regression(t_data.iloc[:, [i, j]], t_data["Viscosity"])
-            if abs(rsqa) > abs(rsqb): features_to_drop.append(i)
-            else: features_to_drop.append(j)
+y = t_data["Viscosity"]
+X = t_data.loc[:, t_data.columns[t_data.columns != "Viscosity"]]
 
-t_data = t_data.loc[:, [not i in features_to_drop for i in range(0, t_data.shape[1])]]
-
-print(t_data.shape)
-exit(0)
-
-rsqs = np.zeros(ranking.shape)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
 # Wybór parametrów
-for n in range(len(ranking)):
-    y = t_data["Viscosity"].to_numpy()
-    X = t_data.loc[:, ranking[:(n+1)]].to_numpy()
+for n in range(1, t_data.shape[1]-1):
+    selected = SequentialFeatureSelector(
+            svm.SVR(), n_features_to_select = n, cv = 5, n_jobs = -1
+            ).fit(X_train, y_train)
 
-    kf = KFold(n_splits=5)
-    scores = np.zeros((5,))
+    clf = svm.SVR().fit(X_train[selected.get_feature_names_out()], y_train)
+    rsqs.append(clf.score(X_test[selected.get_feature_names_out()], y_test))
+    np.save(f"{TRAIN_DIR}/parameter_selection_iter{n}.npy", rsqs)
 
-    for i, (train_ind, test_ind) in enumerate(kf.split(X)):
-        train_X = X[np.array(train_ind), :]
-        train_y = y[np.array(train_ind)]
-        test_X = X[np.array(test_ind)]
-        test_y = y[np.array(test_ind)]
-
-        clf = svm.SVR().fit(train_X, train_y)
-        scores[i] = (clf.score(test_X, test_y))
-
-    rsqs[n] = scores.mean()
-
-print(rsqs.shape)
+print(len(rsqs))
 np.save(f"{TRAIN_DIR}/parameter_selection.npy", rsqs)
 
 print("finished!\a")
